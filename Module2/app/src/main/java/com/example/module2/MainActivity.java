90184741dd87da6881ed6a8cf7dd620060cbf075
YipHoiChun <yiphoichun29@gmail.com>
2021-02-20 10:17:46 +0800

    add api

--- Module2/app/src/main/java/com/example/module2/MainActivity.java
+++ Module2/app/src/main/java/com/example/module2/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.module2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText_email, editText_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText_email = findViewById(R.id.editText_email);
+        editText_password = findViewById(R.id.editText_password);
     }
 }
